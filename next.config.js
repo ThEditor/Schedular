9e62c1317c969ccac0185596723dce75cfc6b9ab
ThEditor <theditormcpe@gmail.com>
2022-02-02 13:10:45 +0530

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   eslint: {
     dirs: ['src'],
   },
 
   reactStrictMode: true,
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public'
+  },
 
   // Uncoment to add domain whitelist
   // images: {
@@ -31,4 +38,4 @@ module.exports = {
 
     return config;
   },
-};
+});
